dc770dc73f87338fa8cb1c8bbb1eca171de42e86
soieu <soieu9898@gmail.com>
2022-05-17 23:01:26 +0900

    버튼만듬

--- screens/tabthree/SearchMuseum.js
+++ screens/tabthree/SearchMuseum.js
@@ -1,44 +1,80 @@
 import * as React from "react";
 import { useState, useEffect } from "react";
 import MapView from "react-native-maps";
-import { StyleSheet, View, Dimensions, SafeAreaView, ActivityIndicator, TextInput, Touchable } from "react-native";
-import { Ionicons } from '@expo/vector-icons';
-import { TouchableOpacity } from "react-native-web";
-
-
+import {
+  StyleSheet,
+  View,
+  Dimensions,
+  SafeAreaView,
+  ActivityIndicator,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import * as Location from "expo-location";
+import { getLocation } from "react-native-element/Library/react-native-element-core/API/ENativeSystemApi";
 
 export default function SearchMuseum() {
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+  const [initialRegion, setInitialRegion] = useState({
+    latitude: 35.91395373474155,
+    longitude: 127.73829440215488,
+    latitudeDelta: 5,
+    longitudeDelta: 5,
+  });
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+
+  let text = "Waiting..";
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location);
+  }
+
+  const getLocation = () => {};
 
   return (
     <SafeAreaView style={styles.root}>
       <View style={styles.container}>
-        <MapView style={styles.map}>
+        <MapView
+          style={styles.map}
+          initialRegion={initialRegion}
+          showsUserLocation={true}
+          showsMyLocationButton={true}
+        >
           <View style={styles.consearchbar}>
-            <TextInput
-              style={styles.SearchB}
-              placeholder = "위치"
-            >
-            </TextInput>
-            <TextInput
-              style={styles.SearchB}
-              placeholder = "검색어"
-            >
-            </TextInput>
+            <TextInput style={styles.SearchB} placeholder="위치"></TextInput>
+            <TextInput style={styles.SearchB} placeholder="검색어"></TextInput>
           </View>
 
-          <View style={styles.historybar}>            
-            
-          </View>
+          <TouchableOpacity activeOpacity={0.7}>
+            <View style={styles.containermyloca}>
+              <Ionicons name="locate-outline" size="30"></Ionicons>
+            </View>
+          </TouchableOpacity>
+
+          <View style={styles.historybar}></View>
         </MapView>
       </View>
     </SafeAreaView>
   );
-  
 }
 
 const styles = StyleSheet.create({
-  root: {
-  },
+  root: {},
   container: {
     backgroundColor: "#fff",
     alignItems: "center",
@@ -46,7 +82,7 @@ const styles = StyleSheet.create({
   },
   consearchbar: {
     justifyContent: "space-around",
-    flexDirection: "row"
+    flexDirection: "row",
   },
   map: {
     display: "flex",
@@ -55,7 +91,7 @@ const styles = StyleSheet.create({
     height: Dimensions.get("window").height,
   },
   SearchB: {
-    backgroundColor : "white",
+    backgroundColor: "white",
     marginTop: 20,
     width: 160,
     height: 45,
@@ -63,27 +99,26 @@ const styles = StyleSheet.create({
     padding: 10,
     borderRadius: 20,
     borderWidth: 1.5,
-
-    
   },
-  searchbutton: {
-
-  },
+  searchbutton: {},
   historybar: {
     height: "50%",
     width: "100%",
     borderRadius: 16,
     backgroundColor: "white",
-    position:"absolute",
-    bottom:-10,
+    position: "absolute",
+    bottom: -10,
     justifyContent: "flex-start",
     alignItems: "flex-start",
     flexDirection: "column",
   },
-  containermyloca:{
+
+  containermyloca: {
     width: 50,
-    height:50,
-    borderRadius:100,
-    backgroundColor:"white",
-  }
-  });
+    height: 50,
+    borderRadius: 100,
+    backgroundColor: "white",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
